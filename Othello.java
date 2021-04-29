f1784f839cfb62c5778b38bd2ee07175dbf09f10
CyrilSharma <44011901+CyrilSharma@users.noreply.github.com>
2021-04-28 21:50:19 -0400

    Finished comments for othello class

--- Othello.java
+++ Othello.java
@@ -3,10 +3,12 @@ public class Othello {
      * This class handles all the game mechanics in Othello.
      * PRIVATE VARIABLES
      * board is an array which keeps track of the state by storing 1 for player 1, -1 for player 2, and 0 for no player at each point on the board
+     * game_history stores all the moves taken in the game, each specified by two coordinates (ex. (1, 0))
      * player is a token to keep track of who's turn it is
     **/
 
     private int[][] board = new int[8][8];
+    private int[][] game_history = new int[8][8];
     private int player = 1;
 
     Othello() {
@@ -14,28 +16,31 @@ public class Othello {
     }
 
     boolean is_win(int action) {
+        // determines from the last action whether the game has been won or not
         return false;
     }
 
     boolean is_draw() {
-        return false;
-    }
-
-    boolean is_terminal(int action) {
+        // determines whether the game is drawn
         return false;
     }
 
     void move(int action) {
+        // advances the game state by the appropiate action, assuming the action is legal
         return;
     }
 
-    void unmove(int action) {
+    void unmove() {
+        // regresses the game state by the appropiate action, assuming the action was legal
         return;
     }
 
 
-    int[][] legal() {
-        int[][] dummy = new int[1][1];
+    boolean[][] legal() {
+        // crafts an array of arrays, where true corresponds to an action being legal, and false corresponds to an illegal action
+
+        // dummy variable to make code compile
+        boolean[][] dummy = new boolean[1][1];
         return dummy;
     }
 }
