8cdd3fbb0855757cb28cbd9c2f6eb3b8c9e04e8d
Sophia Lu <sophialu16@gmail.com>
2021-04-28 22:57:20 -0400

    Added comments

---
+++ Player.java
@@ -0,0 +1,21 @@
+public class Player   {
+
+    private int player_wins = 0; 
+    private int discCount = 0; 
+
+    public setWins()    {
+        return;
+    }
+
+    public getWins()    {
+        return;
+    }
+
+    public set_discCount()  {
+        return;
+    }
+
+    public get_discCount()  {
+        return;
+    }
+}
--- Tournament.java
+++ Tournament.java
@@ -1,4 +1,4 @@
-public class Tournament {
+public class Tournament {   
     /** 
     * This class handles the tournament system in Othello.
     */
@@ -7,16 +7,16 @@ public class Tournament {
 
     private float[] p_scores = {0,0};
 
-    public Tournament(int round_num) {
+    public Tournament(int round_num) {  //Sophia Lu
         this.rounds = round_num;
     }
 
-    float getScore(int player)  {
+    float getScore(int player)  {  //Sophia Lu
         // returns the amount of wins of a player
         return p_scores[player];
     }
 
-    int winner()  {
+    int winner()  { //Sophia Lu
         // check if the tournament is over, and if so return who won
         // 0 means nobody won
         return 0;
