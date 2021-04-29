16e9b29cbdb063b475af3830ee64721d88bc2b30
Sophia Lu <sophialu16@gmail.com>
2021-04-28 21:56:34 -0400

    Tournament stubs

--- Tournament.java
+++ Tournament.java
@@ -1,11 +1,30 @@
 public class Tournament()   {
+    /*
+    This class handles the tournament system in Othello.
 
-    int discCount(int a, int b) {
+    */
+
+    private int player1 = 1; 
+    private int player2 = 2;
+
+    int discCount(int player) {
         return;
     }
 
-    int winCount()  {
+    int winCount(int player)  {
         return;
+    }
+
+    int gameCount() {
+        return;
+    }
+
+    int bestOf(int a)   {
+        return;     //check the number of games the player needs to win
+    }
+
+    boolean winner(int player)  {
+        return;     //check if the player has reached the number of games to win
     }
 
 }
