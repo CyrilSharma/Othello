0920764c29ee6ce89b7eceae9ef888835c6de813
CyrilSharma <44011901+CyrilSharma@users.noreply.github.com>
2021-05-11 22:10:22 -0400

    bug fixes

--- Display.java
+++ Display.java
@@ -3,11 +3,10 @@ import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.*;
 
-public class Display extends JPanel
-{
+public class Display extends JPanel {
   /**
-   * This class displays only the board, 
-   * it does not display any of the interface surrounding the board such as confirmation buttons
+   * This class displays only the board, it does not display any of the interface
+   * surrounding the board such as confirmation buttons
    * 
    * 
    * @param game: a class which handles othello game mechanics
@@ -15,28 +14,25 @@ public class Display extends JPanel
 
   private static final int FRAME = FRAME;
 
-  //fields
+  // fields
   private BufferedImage image;
   private Graphics buffer;
   private ImageIcon game_image;
 
+  private Othello game;
 
-   private Othello game;
-
-   public Display() {
+  public Display() {
     game_image = new ImageIcon("othello.png");
-    image =  new BufferedImage(FRAME, FRAME, BufferedImage.TYPE_INT_ARGB);
+    image = new BufferedImage(FRAME, FRAME, BufferedImage.TYPE_INT_ARGB);
     buffer = image.getGraphics();
-   }
+  }
 
-   public void paintComponent(Graphics g)
-   {
-      buffer.drawImage(game_image.getImage(), 0, 0, FRAME, FRAME, null);
-      move();
-      g.drawImage(image, 0, 0, FRAME, FRAME, null);
-   }
+  public void paintComponent(Graphics g) {
+    drawBoardToBuffer();
+    g.drawImage(image, 0, 0, FRAME, FRAME, null);
+  }
 
-   public void drawBoardToBuffer() {
+  public void drawBoardToBuffer() {
     buffer.drawImage(game_image.getImage(), 0, 0, FRAME, FRAME, null);
 
     int state;
@@ -47,32 +43,33 @@ public class Display extends JPanel
 
         if (state != 0)
           p = new Piece(i, j, 10, state);
-          p.draw(buffer);
+        p.draw(buffer);
       }
     }
 
     repaint();
   }
 
-   public void move(int[] action) { // Cyril Sharma
+  public void move(int[] action) { // Cyril Sharma
     // Advances the internal game state, and updates the display
-    // Also in charge of determining if move is legal 
+    // Also in charge of determining if move is legal
     // TBD: might probably trigger a pop-up to confirm the move
     Piece p = new Piece(action[0], action[1], 100, 1);
     p.draw(buffer);
     repaint();
-   }
+  }
 
-   private void reset() {   //Sophia Lu 
-    // when the game is over, call this method if the player wishes to proceed to a new game
+  private void reset() { // Sophia Lu
+    // when the game is over, call this method if the player wishes to proceed to a
+    // new game
     return;
- }
-   private class ClickListener implements ActionListener // Cyril Sharma
-   {
+  }
+
+  private class ClickListener implements ActionListener // Cyril Sharma
+  {
     // This will call the move method whenever a square is clicked
-      public void actionPerformed(ActionEvent e)
-      {
-        move();
-      }
-   }
+    public void actionPerformed(ActionEvent e) {
+      move();
+    }
+  }
 }
